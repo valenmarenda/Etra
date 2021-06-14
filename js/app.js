79acee755df0fb4d3cac874addca261a3aed954e
valentina <valentinamarenda1@gmail.com>
2021-06-14 08:42:56 -0300

    segundo commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modall");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+
+previews.forEach((preview)=>{
+  preview.addEventListener("click",()=>{
+       modal.classList.add("open");
+       original.classList.add("open");
+       const originalSrc = preview.getAttribute('data-original')
+       original.src = `/img/${originalSrc}`
+   })
+});
+
+modal.addEventListener("click", (e)=> {
+    if (e.target.classList.contains("modall")){
+        modal.classList.remove("open");
+        original.classList.remove("open")
+    }
+})
+
